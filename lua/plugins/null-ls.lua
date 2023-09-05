ed5433c4b0c120661a9a098250661be5c24a451e
vvh413 <nnenekit@gmail.com>
2023-09-05 04:21:27 +0300

    null-ls shfmt args

---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,11 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function(_, opts)
+    local nls = require("null-ls")
+    opts.sources = vim.list_extend(opts.sources, {
+      nls.builtins.formatting.shfmt.with({
+        extra_args = { "-i", "2", "-ci", "-bn", "-s" },
+      }),
+    })
+  end,
+}
