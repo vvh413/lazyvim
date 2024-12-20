c13cd1288c4b1f59bcc6d46e65be36897afbeac2
vvh <ns@ywa.red>
2024-12-20 18:37:10 +0300

    chore: upgrade + enabled clangd extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
