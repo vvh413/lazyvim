6f6915c43d369e48b3c20ad57a70b4db4347d136
vvh <ns@ywa.red>
2025-03-12 15:09:28 +0300

    feat: Markdown auto-formatting + deps update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,6 +8,30 @@ return {
       ruff_format = {
         prepend_args = { "--line-length", "120" },
       },
+      ["markdown-toc"] = {
+        condition = function(_, ctx)
+          for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+            if line:find("<!%-%- toc %-%->") then
+              return true
+            end
+          end
+        end,
+      },
+      ["markdownlint-cli2"] = {
+        condition = function(_, ctx)
+          local diag = vim.tbl_filter(function(d)
+            return d.source == "markdownlint"
+          end, vim.diagnostic.get(ctx.buf))
+          return #diag > 0
+        end,
+      },
+      prettier = {
+        prepend_args = { "--prose-wrap", "always" },
+      },
+    },
+    formatters_by_ft = {
+      ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+      ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
     },
   },
 }
