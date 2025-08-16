1ec860b6f6f2e2b55a0fe80dae2e255af5c9dc44
vvh <ns@ywa.red>
2025-08-16 15:53:31 +0300

    fix: fixed rust-analyzer async-trait error

---
+++ lua/plugins/rustaceanvim.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "mrcjkb/rustaceanvim",
+    opts = {
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            procMacro = {
+              ignored = {
+                ["async-trait"] = vim.NIL,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
